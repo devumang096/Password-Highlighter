59d676025fdc9e28d40232cfc27fca687bf5c2ff
devumang096 <devumang096@gmail.com>
2022-01-17 22:50:07 +0530

    updated files

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ root.addEventListener('mousemove',(e) => {
     let mouseX=rect.right + (rect.width/2);
     let mouseY=rect.top + (rect.height/2);
     let rad=Math.atan2(mouseX-e.pageX,mouseY-e.pageY);
-    let degrees=(rad*(20/Math.PI)*-1)-350;
+    let degrees=(rad*(20/Math.PI)* -1 ) -350;
 
     root.style.setProperty('--beamDegrees', `${degrees}deg`);
 });
 
-eye.addEventListener('click',e =>{
+eye.addEventListener('click',(e) =>{
     e.preventDefault();
     document.body.classList.toggle('show-password');
-    passwordInput.type=passwordInput.type === 'password' ? 'text':'password'
+    passwordInput.type= passwordInput.type === 'password' ? 'text':'password'
     passwordInput.focus();
 });
